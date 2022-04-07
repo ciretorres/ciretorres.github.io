a6de3677173e0c15b45c0cbd1afe055c9d940792
ciretorres <erictorres.velasco@gmail.com>
2022-04-07 11:48:25 -0500

    adding gravity tree to public

--- src/views/Design.vue
+++ src/views/Design.vue
@@ -3,21 +3,13 @@
     <div class="container p-3">
       <p class="color-red">[Visualization Design]</p>
       <ul>
-        <li>
-          Interactive-Interface <a href="https://github.com/ciretorres/thesis-project" target="_blank" class="color-yellow">[UAM-A]</a>
-        </li>
-        <li>
-          AR-System UNIVERSUM <a href="https://ciretorres.github.io/AR-System-UNIVERSUM/" target="_blank" class="color-yellow">[UIC]</a>
-        </li>
-        <li>
-          Solid Waste System's Interactive <a href="https://ciretorres.github.io/flujo-de-residuos-solidos-cdmx/" target="_blank" class="color-yellow">[CDMX]</a>
-        </li>
-        <li>
-          3D Gamma-Ray Catalogue <a href="https://github.com/ciretorres/2HAWC" target="_blank" class="color-yellow">[HAWC Observatory]</a>
-        </li>
-        <li>
-          The Gravity Apple Tree <a href="@/assets/imgs/Gravity-Tree_07112018.jpg" target="_blank" class="color-yellow">[UAM-C]</a>
-        </li>
+        <li>Interactive-Interface <a href="https://github.com/ciretorres/thesis-project" target="_blank" class="color-yellow">[UAM-A]</a></li>
+        <li>AR-System UNIVERSUM <a href="https://ciretorres.github.io/AR-System-UNIVERSUM/" target="_blank" class="color-yellow">[UIC]</a></li>
+        <li>Solid Waste System's Interactive <a href="https://ciretorres.github.io/flujo-de-residuos-solidos-cdmx/" target="_blank" class="color-yellow">[CDMX]</a></li>
+        <li>3D Gamma-Ray Catalogue <a href="https://github.com/ciretorres/2HAWC" target="_blank" class="color-yellow">[HAWC Observatory]</a></li>
+        <!-- <li>The Gravity Apple Tree <a href="@/assets/imgs/2018-11-07-gravity-apple-tree.jpg" target="_blank" class="color-yellow">[UAM-C]</a></li> -->
+        <!-- <li>The Gravity Apple Tree <a href="imgs/2018-11-07-gravity-apple-tree.jpg" target="_blank" class="color-yellow">[UAM-C]</a></li> -->
+        <li>The Gravity Apple Tree <a :href="`${public_path}imgs/2018-11-07-gravity-apple-tree.jpg`" target="_blank" class="color-yellow">[UAM-C]</a></li>
       </ul>
       <router-link class="color-red" to="/">[Back]</router-link>
     </div>
@@ -27,6 +19,11 @@
 <script>
 export default {
   name: 'Design',
+  data() {
+    return {
+      public_path: process.env.BASE_URL
+    }
+  }
 }
 </script>
 
