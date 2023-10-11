9e108c879e6b3830003bffec2c7a349590e6f96e
ciretorres <erictorres.velasco@gmail.com>
2023-10-11 14:16:59 -0600

    composition api y limpieza

--- src/views/development/AreaView.vue
+++ src/views/development/AreaView.vue
@@ -46,19 +46,60 @@ const datosArea = computed(() => {
   })
   return data.value
 })
+
+import Area2 from '@/components/visualizations/area/AreaComponent2.vue'
+import Variables from '../../../public/data/area/dummy_voc.json'
+
+let dict_meses = {
+  ene: '01',
+  feb: '02',
+  mar: '03',
+  abr: '04',
+  may: '05',
+  jun: '06',
+  jul: '07',
+  ago: '08',
+  sep: '09',
+  oct: '10',
+  nov: '11',
+  dic: '12',
+}
+let dict_meses_invert = {}
+Object.keys(dict_meses).map(d => (dict_meses_invert[dict_meses[d]] = d))
+
+Variables.map(d => {
+  if (d.fecha_1.includes('/')) {
+    let fecha_sep = d.fecha_1.split('/')
+    d.fecha_1 = [fecha_sep[0], dict_meses[fecha_sep[1]], fecha_sep[2]].join('-')
+    fecha_sep = d.fecha_2.split('/')
+    d.fecha_2 = [fecha_sep[0], dict_meses[fecha_sep[1]], fecha_sep[2]].join('-')
+  }
+})
+
+const datos_grafica = ref([])
+datos_grafica.value = [...Variables]
 </script>
 
 <template>
   <div class="area-view">
     <div class="container sin-fondo">
+      <Area2
+        areas_apiladas_id="streamgraphbasico"
+        :alto_vis="300"
+        :datos="datos_grafica"
+        :tooltip_activo="true"
+        :variables="[
+          { id: 'Variable_1', nombre: 'Variable_1', color: '#C2E7D9' },
+          { id: 'Variable_2', nombre: 'Variable_2', color: '#A6CFD5' },
+          { id: 'Variable_3', nombre: 'Variable_3', color: '#26408B' },
+          { id: 'Variable_4', nombre: 'Variable_4', color: '#0F084B' },
+          { id: 'Variable_5', nombre: 'Variable_5', color: '#0D0221' },
+        ]"
+        nombre_columna_horizontal="fecha_1"
+      />
       <Area
         :area_id="'area'"
         :datos="datosArea"
-        :titulo_proyecto="'Título/enlace del proyecto'"
-        fecha_actualizacion="dd/mm/aaaa"
-        titulo_eje_x="Título eje x"
-        titulo_eje_y="Título eje y"
-        titulo_leyenda="Título de leyenda"
         color_area="#fff"
       />
     </div>
