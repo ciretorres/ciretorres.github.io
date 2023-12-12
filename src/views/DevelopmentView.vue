48942080db34103981906c8963c196830610a2c4
ciretorres <erictorres.velasco@gmail.com>
2023-12-12 14:26:44 -0600

    colocando de abrir y cerrar por proyecto

--- src/views/DevelopmentView.vue
+++ src/views/DevelopmentView.vue
@@ -1,234 +1,252 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-const mostrar = ref(true)
+// const mostrar = ref(true)
+const mostrarProyectos = ref(false)
 </script>
 
 <template>
-  <main
-    id="principal"
-    class="development"
-  >
+  <main id="principal" class="development">
     <div class="container p-2">
       <p class="color-red">[Other stuff...]</p>
       <ul>
         <li>
-          <a
-            @click="mostrar = !mostrar"
-            style="cursor: pointer"
-            class="color-white"
-            rel="noopener noreferrer"
-          >
+          <a class="" rel="noopener noreferrer">
             Web Frontend <span class="color-yellow">[Conahcyt]</span></a
           >
         </li>
-        <ul v-show="mostrar">
+        <ul>
           <li>
+            <a
+              href="https://gema.conahcyt.mx/"
+              class="color-white"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Generador y visualizador de Mapas</a
+            >
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              href="https://sisdai.conahcyt.mx/"
+              class="color-white"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Sistema de diseño y Accesibilidad para la Investigación</a
+            >
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <span
+              @click="mostrarProyectos = !mostrarProyectos"
+              class="color-white"
+              style="cursor: pointer"
+              >Ecosistemas Nacionales Informáticos,</span
+            >
+
             <a
               href="https://eni.conacyt.mx/"
               class="color-white"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Ecosistemas Nacionales Informáticos</a
+              ENI</a
             >
           </li>
-          <ul>
-            <li>
-              <a
-                href="https://salud.conacyt.mx/"
-                class="color-white"
-                target="_blank"
-                rel="noopener noreferrer"
-                >Salud</a
-              >
-            </li>
+          <span v-show="mostrarProyectos">
             <ul>
               <li>
                 <a
-                  href="https://salud.conacyt.mx/vih/"
+                  href="https://salud.conacyt.mx/"
                   class="color-white"
                   target="_blank"
                   rel="noopener noreferrer"
-                  >VIH</a
-                >
-              </li>
-              <li>
-                <a
-                  href="https://salud.conacyt.mx/coronavirus/"
-                  class="color-white"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >Coronavirus</a
+                  >Salud</a
                 >
               </li>
               <ul>
                 <li>
                   <a
-                    href="https://salud.conacyt.mx/coronavirus/variantes/"
+                    href="https://salud.conacyt.mx/vih/"
                     class="color-white"
                     target="_blank"
                     rel="noopener noreferrer"
-                    >Variantes</a
+                    >VIH</a
                   >
                 </li>
                 <li>
                   <a
-                    href="https://salud.conacyt.mx/coronavirus/vacunacion/"
+                    href="https://salud.conacyt.mx/coronavirus/"
                     class="color-white"
                     target="_blank"
                     rel="noopener noreferrer"
-                    >Vacunacion</a
+                    >Coronavirus</a
+                  >
+                </li>
+                <ul>
+                  <li>
+                    <a
+                      href="https://salud.conacyt.mx/coronavirus/variantes/"
+                      class="color-white"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >Variantes</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="https://salud.conacyt.mx/coronavirus/vacunacion/"
+                      class="color-white"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >Vacunacion</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      class="color-white"
+                      href="https://salud.conahcyt.mx/coronavirus/investigacion/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >Investigacion</a
+                    >
+                  </li>
+                </ul>
+              </ul>
+            </ul>
+            <ul>
+              <li>
+                <a
+                  href="https://alimentacion.conacyt.mx/"
+                  class="color-white"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Soberanía alimentaria</a
+                >
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="https://alimentacion.conacyt.mx/glifosato/"
+                    class="color-white"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >Glifosato</a
                   >
                 </li>
                 <li>
                   <a
+                    href="https://alimentacion.conacyt.mx/piesagiles/"
                     class="color-white"
-                    href="https://salud.conahcyt.mx/coronavirus/investigacion/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    >Investigacion</a
+                    >Pies ágiles</a
                   >
                 </li>
               </ul>
             </ul>
-          </ul>
-          <ul>
-            <li>
-              <a
-                href="https://alimentacion.conacyt.mx/"
-                class="color-white"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Soberanía alimentaria</a
-              >
-            </li>
+            <ul>
+              <li>Socioecología</li>
+              <ul>
+                <li>
+                  <a
+                    href="https://socioecologia.conacyt.mx/texcoco/"
+                    class="color-white"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >Texcoco</a
+                  >
+                </li>
+              </ul>
+            </ul>
             <ul>
               <li>
                 <a
-                  href="https://alimentacion.conacyt.mx/glifosato/"
+                  href="https://energia.conacyt.mx/"
                   class="color-white"
                   target="_blank"
                   rel="noopener noreferrer"
-                  >Glifosato</a
+                  >Energía y cambio climático</a
                 >
               </li>
-              <li>
-                <a
-                  href="https://alimentacion.conacyt.mx/piesagiles/"
-                  class="color-white"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >Pies ágiles</a
-                >
-              </li>
+              <ul>
+                <li>
+                  <a
+                    href="https://energia.conacyt.mx/sargazo/"
+                    class="color-white"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >Sargazo</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="https://energia.conacyt.mx/planeas/"
+                    class="color-white"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >Planeas</a
+                  >
+                </li>
+              </ul>
             </ul>
-          </ul>
-          <ul>
-            <li>Socioecología</li>
-            <ul>
-              <li>
-                <a
-                  href="https://socioecologia.conacyt.mx/texcoco/"
-                  class="color-white"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >Texcoco</a
-                >
-              </li>
-            </ul>
-          </ul>
-          <ul>
-            <li>
-              <a
-                href="https://energia.conacyt.mx/"
-                class="color-white"
-                target="_blank"
-                rel="noopener noreferrer"
-                >Energía y cambio climático</a
-              >
-            </li>
-            <ul>
-              <li>
-                <a
-                  href="https://energia.conacyt.mx/sargazo/"
-                  class="color-white"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >Sargazo</a
-                >
-              </li>
-              <li>
-                <a
-                  href="https://energia.conacyt.mx/planeas/"
-                  class="color-white"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >Planeas</a
-                >
-              </li>
-            </ul>
-          </ul>
-          <ul>
-            <li>
-              <a
-                class="color-white"
-                href="https://agua.conahcyt.mx/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >Agua</a
-              >
-            </li>
-            <ul>
-              <li>
-                <a
-                  href="https://agua.conacyt.mx/cuencas/"
-                  class="color-white"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >Cuencas</a
-                >
-              </li>
-            </ul>
-          </ul>
-          <ul>
-            <li>
-              <a
-                class="color-white"
-                href="https://toxicologia.conacyt.mx/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >Agentes Tóxicos y Procesos Contaminantes</a
-              >
-            </li>
             <ul>
               <li>
                 <a
                   class="color-white"
-                  href="https://toxicologia.conahcyt.mx/resa-atoyac/"
+                  href="https://agua.conahcyt.mx/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  >RESA: Cuenca del alto Atoyac</a
+                  >Agua</a
                 >
               </li>
+              <ul>
+                <li>
+                  <a
+                    href="https://agua.conacyt.mx/cuencas/"
+                    class="color-white"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >Cuencas</a
+                  >
+                </li>
+              </ul>
             </ul>
-          </ul>
+            <ul>
+              <li>
+                <a
+                  class="color-white"
+                  href="https://toxicologia.conacyt.mx/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >Agentes Tóxicos y Procesos Contaminantes</a
+                >
+              </li>
+              <ul>
+                <li>
+                  <a
+                    class="color-white"
+                    href="https://toxicologia.conahcyt.mx/resa-atoyac/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >RESA: Cuenca del alto Atoyac</a
+                  >
+                </li>
+              </ul>
+            </ul>
+          </span>
         </ul>
+
         <li>
-          <router-link
-            class="color-white"
-            to="/development/visualization/intro"
-          >
+          <router-link class="color-white" to="/development/visualization/intro">
             Go to <span class="color-yellow">[Visualization]</span></router-link
           >
         </li>
       </ul>
-      <router-link
-        class="color-red"
-        to="/"
-        >[Back]</router-link
-      >
+      <router-link class="color-red" to="/">[Back]</router-link>
     </div>
   </main>
 </template>
