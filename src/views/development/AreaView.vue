31c311cc2916cc9f9631a1b6b7fcc4431d1bd31e
ciretorres <erictorres.velasco@gmail.com>
2024-11-12 21:18:36 -0600

    fix: axios datos viz area

--- src/views/development/AreaView.vue
+++ src/views/development/AreaView.vue
@@ -1,36 +1,37 @@
 <script setup>
-import axios from 'axios';
-import * as d3 from 'd3';
-import { computed, onMounted, ref } from 'vue';
+// import axios from 'axios'
+import * as d3 from 'd3'
+import { computed, onMounted, ref } from 'vue'
 
-import Area from '@/components/visualizations/area/AreaComponent.vue';
+import Area from '@/components/visualizations/area/AreaComponent.vue'
+import datosA from '../../../public/data/area/area.json'
 
 // const public_path = process.env.BASE_URL
-const public_path = import.meta.env.BASE_URL
+// const public_path = import.meta.env.BASE_URL
 
-const data = ref([])
-const errored = ref(false)
-const loading = ref(true)
+const data = ref(datosA)
+// const errored = ref(false)
+// const loading = ref(true)
 
 onMounted(() => {
   // api request
-  axios
-    .get(public_path + 'data/area/area.json')
-    .then(response => {
-      // handle data array here
-      data.value = response.data
-    })
-    .catch(error => {
-      console.log(error)
-      errored.value = true
-    })
-    .finally(() => {
-      loading.value = true
-    })
+  // axios
+  //   .get(datosA)
+  //   .then((response) => {
+  //     // handle data array here
+  //     data.value = response.data
+  //   })
+  //   .catch((error) => {
+  //     console.log(error)
+  //     errored.value = true
+  //   })
+  //   .finally(() => {
+  //     loading.value = true
+  //   })
 })
 
 const datosArea = computed(() => {
-  data.value.forEach(d => {
+  data.value.forEach((d) => {
     // Parse data
     d.date = d3.timeParse('%Y-%m-%d')(d.date)
     d.value = +d.value
@@ -44,10 +45,7 @@ const datosArea = computed(() => {
   <div class="area-view">
     <div class="container sin-fondo">
       <h1>Area</h1>
-      <Area
-        :area_id="'area'"
-        :datos="datosArea"
-      />
+      <Area :datos="datosArea" />
     </div>
   </div>
 </template>
