5ad8b3ca0783430196585e63a8aa99bda538b947
ciretorres <erictorres.velasco@gmail.com>
2026-09-08 21:25:49 -0600

    feat: instalando scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,7 +58,7 @@ export default defineNuxtConfig({
     enabled: true
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/style/main.scss'],
 
   routeRules: {
     '/': { prerender: true }
