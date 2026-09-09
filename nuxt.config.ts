a048a9db0e9a1af61e03aba1315172ba54f29897
ciretorres <erictorres.velasco@gmail.com>
2026-09-08 21:10:36 -0600

    feat: agregando metadata y estilos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,56 @@
+const title = 'ciretorres'
+const description = ''
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/',
+    head: {
+      link: [
+        {
+          rel: 'shortcut icon',
+          href: '/favicon.ico',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        },
+      ],
+      meta: [
+        { charset: 'UTF-8', },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'keywords', content: '' },
+        { name: 'description', content: description },
+        { property: 'og:type', content: 'article' },
+        { property: 'og:title', content: title },
+        { property: 'og:description', content: description, },
+        { property: 'og:image', content: '/img/icono.png', },
+        // { name: 'twitter:card', content: 'summary' },
+        // { name: 'twitter:title', content: 'ciretorres' },
+        // {
+        //   name: 'twitter:description',
+        //   content: "metaDescription",
+        // },
+        // {
+        //   name: 'twitter:image',
+        //   content: '/img/icono_sigic.png',
+        // },
+      ],
+      htmlAttrs: {
+        lang: 'es-mx'
+      }
+    },
+  },
+
   modules: [
+    // '@pinia/nuxt',
     '@nuxt/eslint',
+    // '@nuxt/test-utils',
     // '@nuxt/ui'
   ],
 
