376eaa5df286482459b9d6ec25e3f8a82195f365
ciretorres <erictorres.velasco@gmail.com>
2026-09-10 20:54:29 -0600

    feat: agregando area

--- app/pages/visualization/area-diagram.vue
+++ app/pages/visualization/area-diagram.vue
@@ -1,3 +1,27 @@
+<script setup>
+import * as d3 from 'd3'
+import { ref } from 'vue'
+
+import datosA from '@/assets/data/area.json'
+
+const data = ref(datosA)
+
+// Parse data para Área
+const datosArea = computed(() => {
+  data.value.forEach((d) => {
+    d.date = d3.timeParse('%Y-%m-%d')(d.date)
+    d.value = +d.value
+  })
+  return data.value
+})
+// console.log(datosArea.value)
+</script>
+
 <template>
-  <div><h3>Area</h3></div>
+  <article class="">
+    <h3>Area</h3>
+    <ClientOnly>
+      <AreaComponent :datos="datosArea" />
+    </ClientOnly>
+  </article>
 </template>
